ecb23867fa48b938e761acff493cffdab241c890
Fabio <fasfort@gmail.com>
2025-08-25 17:16:59 -0300

    Serviços

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -22,19 +22,7 @@ const Services = () => {
           )
         })}
       </div>
-      <br />
-      <div className="services__container container">
-        {services.map(({ name, title, description }, index) => {
-          return(
-            <div className="services__item card card-one" key={index}>
-              <span className="services__subtitle text-cs">{name}</span>
-
-              <h3 className="services__title">{title}</h3>
-              <p className="services__description">{description}</p>
-            </div>
-          )
-        })}
-      </div>
+      
 
       {/* <div className="section__deco deco__right"></div> */}
     </section>
