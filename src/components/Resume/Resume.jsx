a4cb0340a97d83edd53daf0a94d7aec07423a2a0
Fabio <fasfort@gmail.com>
2025-08-25 17:21:36 -0300

    Serviços32

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -33,28 +33,9 @@ const Resume = () => {
                 </div>
             </div>
 
-            <div className="resume__group">
-                <h3 className="resume__heading">Experiência</h3>
+            
 
-                <div className="resume__items">
-                    {cv.map((val, id) => {
-                        if(val.category === 'experiencia') {
-                            return (
-                                <Card 
-                                    key={id} 
-                                    title={val.title} 
-                                    subtitle={val.subtitle} 
-                                    date={val.date} 
-                                    description={val.description} 
-                                />
-                            );
-                        }
-                    })}
-                </div>
-            </div>
-        </div>
-
-        {/* <div className="section__deco deco__left"></div> */}
+        <div className="section__deco deco__left"></div>
     </section>
   );
 };
